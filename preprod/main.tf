9deb1dc3209eca6bca38ec57b75b523c8f5e3111
Pragya Dubey <pragyadubey@Mac.communityfibre.co.uk>
2025-07-31 22:56:06 +0100

    code added for subnet

--- preprod/main.tf
+++ preprod/main.tf
@@ -11,8 +11,19 @@ module "virtual_network" {
 
   source = "../modules/azurerm_virtual_network"
   vnet_name = "secure_vnet_for_ai"
-  ip_address_range = "[10.0.0.0/16]"
+  ip_address_range = ["10.0.0.0/16"]
   resource_group_name = "rg-aiapp"
   location = "uksouth"
+  
+}
+
+
+module "frontend_subnet" {
+
+  source = "../modules/azurerm_subnet"
+  subnet_name = "frontend_subnet"
+  vnet_name = "secure_vnet_for_ai"
+  resource_group_name = "rg-aiapp"
+  address_prefixes = ["10.0.1.0/24"]
   
 }
